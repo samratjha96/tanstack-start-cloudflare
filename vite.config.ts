48ad54fe5429b31f9699060063d6b142ca25a19a
Matthew Sessions <matthewlsessions@gmail.com>
2025-09-26 13:13:40 -0600

    added ssr config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-// import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
@@ -18,11 +18,11 @@ const config = defineConfig({
       server: { entry: "./server.ts" },
     }),
     viteReact(),
-    // cloudflare({
-    //   viteEnvironment: {
-    //     name: "ssr",
-    //   },
-    // }),
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
   ],
 });
 
